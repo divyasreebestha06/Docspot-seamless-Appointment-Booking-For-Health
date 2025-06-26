3ae529b1c81a5bf2abb4e9ccdafb3bdd9bfea0ba
divyasreebestha06 <divyasreebestha161@gmail.com>
2025-06-26 19:06:46 +0530

    Add files via upload

--- AppContext.jsx
+++ AppContext.jsx
@@ -1,34 +1,73 @@
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from 'axios'
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currency = import.meta.env.VITE_CURRENCY
+    const currencySymbol = '₹'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
+    const [userData, setUserData] = useState(false)
 
-    // Function to format the date eg. ( 20_01_2000 => 20 Jan 2000 )
-    const slotDateFormat = (slotDate) => {
-        const dateArray = slotDate.split('_')
-        return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    // Getting Doctors using API
+    const getDoctosData = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/doctor/list')
+            if (data.success) {
+                setDoctors(data.doctors)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
     }
 
-    // Function to calculate the age eg. ( 20_01_2000 => 24 )
-    const calculateAge = (dob) => {
-        const today = new Date()
-        const birthDate = new Date(dob)
-        let age = today.getFullYear() - birthDate.getFullYear()
-        return age
+    // Getting User Profile using API
+    const loadUserProfileData = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+
+            if (data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
     }
+
+    useEffect(() => {
+        getDoctosData()
+    }, [])
+
+    useEffect(() => {
+        if (token) {
+            loadUserProfileData()
+        }
+    }, [token])
 
     const value = {
+        doctors, getDoctosData,
+        currencySymbol,
         backendUrl,
-        currency,
-        slotDateFormat,
-        calculateAge,
+        token, setToken,
+        userData, setUserData, loadUserProfileData
     }
 
     return (
